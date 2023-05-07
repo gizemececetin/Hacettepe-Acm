4943739de0a687d4e1a733739f310a259f315b46
Gizem Cetin <gizem.cetin@wearelayer.com>
2023-05-07 23:58:53 +0300

    İkinci ders kodları

---
+++ flutter/first_project/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() => runApp(const FirstProject());
+
+class FirstProject extends StatelessWidget {
+  const FirstProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Project",
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
